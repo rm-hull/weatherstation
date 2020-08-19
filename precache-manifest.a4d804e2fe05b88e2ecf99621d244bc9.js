ce56985eda0f2bfd5d64cd79ac1c032f2161c3c8
Richard Hull <rm_hull@yahoo.co.uk>
2020-08-19 18:27:46 +0100

    Updates

---
+++ precache-manifest.a4d804e2fe05b88e2ecf99621d244bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf49b48ee71cce74d4af5ba052ef37e",
+    "url": "/weatherstation/index.html"
+  },
+  {
+    "revision": "2f2b005a6ab5801af93d",
+    "url": "/weatherstation/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4427d724b3aa7a495766",
+    "url": "/weatherstation/static/js/2.b7839b00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weatherstation/static/js/2.b7839b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2b005a6ab5801af93d",
+    "url": "/weatherstation/static/js/main.9413b865.chunk.js"
+  },
+  {
+    "revision": "de8cfff92d1619e1db81",
+    "url": "/weatherstation/static/js/runtime-main.b52dd4f3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61de5e0a363c290e196f26a39f932837.js"
+  "/weatherstation/precache-manifest.a4d804e2fe05b88e2ecf99621d244bc9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weatherstation/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
